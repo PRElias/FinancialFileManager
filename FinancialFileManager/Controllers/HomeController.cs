74c302c0c6a47499ecd6c8924e1a59be88f67c8b
Pepeu <ppuspfc@gmail.com>
2017-06-13 16:18:15 -0300

    commit inicial

--- FinancialFileManager/Controllers/HomeController.cs
+++ FinancialFileManager/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +26,53 @@ namespace FinancialFileManager.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Upload()
+        {
+            ViewBag.Message = "Escolha o arquivo";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine("C:\\Arquivos", fileName);
+                    file.SaveAs(path);
+                }
+                ViewBag.Message = "Upload successful";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ViewBag.Message = "Upload failed";
+                return RedirectToAction("Uploads");
+            }
+        }
+
+        public ActionResult Downloads()
+        {
+            var dir = new System.IO.DirectoryInfo("C:\\Arquivos");
+            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*");
+            List<string> items = new List<string>();
+
+            foreach (var file in fileNames)
+            {
+                items.Add(file.Name);
+            }
+
+            return View(items);
+        }
+
+        public FileResult Download(string FileName)
+        {
+            return File("C:\\Arquivos" + FileName, System.Net.Mime.MediaTypeNames.Application.Octet);
         }
     }
 }
